a270fba0db0c31e2d0f7586f31886cdf46a8dbc4
adumbidiot <nathaniel.daniel23@outlook.com>
2020-12-03 14:34:56 -0800

    Create basic framework for state handling

--- CPE301-Team-Project.ino
+++ CPE301-Team-Project.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+enum STATE { DISABLED = 0, IDLE = 1, RUNNING = 2, ERROR = 3 };
 
+volatile STATE state = DISABLED;
+
+void setup() {
+  // other setup...
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  switch (state) {
+  case DISABLED:
+    // disabled handling...
+    break;
+  case IDLE:
+    // idle handling...
+    break;
+  case RUNNING:
+    // running handling...
+    break;
+  case ERROR:
+    // error handling...
+    break;
+  default:
+    state = DISABLED;
+  }
 }
