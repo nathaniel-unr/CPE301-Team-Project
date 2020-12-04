244ffa9db7396a78f201daa7c048a2810beb5e6f
Nathaniel Daniel <nathanieldaniel@nevada.unr.edu>
2020-12-03 16:23:59 -0800

    Add Status Leds

--- CPE301-Team-Project.ino
+++ CPE301-Team-Project.ino
@@ -13,6 +13,10 @@
 
 #define IS_HIGH(port, pin) ((*port & (0x01 << pin)) != 0)
 
+DECL_REG_U8(myPORTA, 0x22);
+DECL_REG_U8(myDDRA, 0x21);
+DECL_REG_U8(myPINA, 0x20);
+
 DECL_REG_U8(myPORTB, 0x25);
 DECL_REG_U8(myDDRB, 0x24);
 DECL_REG_U8(myPINB, 0x23);
@@ -31,6 +35,11 @@ DECL_REG_U8(myTIFR1, 0x36);
 
 #define BUTTON_PIN 4
 
+#define YELLOW_LED_PIN 1
+#define GREEN_LED_PIN 3
+#define BLUE_LED_PIN 5
+#define RED_LED_PIN 7
+
 enum STATE {
   DISABLED_STATE = 0,
   IDLE_STATE = 1,
@@ -42,43 +51,85 @@ volatile STATE state = DISABLED_STATE;
 
 bool last_button_state = false;
 
-void setup() {
-  // other setup...
+void change_state(STATE new_state) {
+  switch (state) {
+  default:
+  case DISABLED_STATE:
+    SET_LOW(myPORTA, YELLOW_LED_PIN);
+    break;
+  case IDLE_STATE:
+    SET_LOW(myPORTA, GREEN_LED_PIN);
+    break;
+  case RUNNING_STATE:
+    SET_LOW(myPORTA, BLUE_LED_PIN);
+    break;
+  case ERROR_STATE:
+    SET_LOW(myPORTA, RED_LED_PIN);
+    break;
+  }
 
+  state = new_state;
+
+  switch (new_state) {
+  default:
+  case DISABLED_STATE:
+    SET_HIGH(myPORTA, YELLOW_LED_PIN);
+    break;
+  case IDLE_STATE:
+    SET_HIGH(myPORTA, GREEN_LED_PIN);
+    break;
+  case RUNNING_STATE:
+    SET_HIGH(myPORTA, BLUE_LED_PIN);
+    break;
+  case ERROR_STATE:
+    SET_HIGH(myPORTA, RED_LED_PIN);
+    break;
+  }
+}
+
+void setup() {
   SET_INPUT(myDDRC, BUTTON_PIN);
   ENABLE_PULLUP(myPORTC, BUTTON_PIN);
 
+  SET_OUTPUT(myDDRA, YELLOW_LED_PIN);
+  SET_OUTPUT(myDDRA, GREEN_LED_PIN);
+
   Serial.begin(9600);
+
+  change_state(DISABLED_STATE);
 }
 
 void loop() {
   bool current_button_state = !IS_HIGH(myPINC, BUTTON_PIN);
 
+  // TODO: Debounce. USe ISR?
   if (current_button_state && current_button_state != last_button_state) {
     if (state != DISABLED_STATE) {
-      state = DISABLED_STATE;
+      change_state(DISABLED_STATE);
     } else {
-      state = IDLE_STATE;
+      change_state(IDLE_STATE);
     }
   }
   last_button_state = current_button_state;
 
   switch (state) {
   case DISABLED_STATE:
+    Serial.println("DISABLED");
     // disabled handling...
-    Serial.println("Disabled");
     break;
   case IDLE_STATE:
+    Serial.println("IDLE");
     // idle handling...
-    Serial.println("Idle");
     break;
   case RUNNING_STATE:
+    Serial.println("RUNNING");
     // running handling...
     break;
   case ERROR_STATE:
+    Serial.println("ERROR");
     // error handling...
     break;
   default:
-    state = DISABLED_STATE;
+    change_state(DISABLED_STATE);
   }
 }
